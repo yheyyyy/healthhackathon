79a7d7ddd81e8f326b5c638d2eaf7f55e4aa1702
Onn Yun Hui <onn_yun_hui@aiap.sg>
2025-03-02 21:32:10 +0800

    Add logo, session state management, and sample image processing in Streamlit app

--- hackathon/streamlit.py
+++ hackathon/streamlit.py
@@ -1,9 +1,24 @@
 import streamlit as st
 from unified_agent import process_message
 from ocr_tool import ocr_tool_function
+from PIL import Image
+
+
+# Initialize session state
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "preview_message" not in st.session_state:
+    st.session_state.preview_message = None
+    
+# Load the logo and face images
+logo = Image.open("imgs/logo.png")
+assist_face = "imgs/face.png"
+user_face = "imgs/face2.png"
+
 def chatbot():
+    st.logo(logo, size="large")
     st.title("AI-Powered Healthcare Assistant")
-    st.sidebar.markdown("### Example Prompts")
+    st.sidebar.markdown("### Demo Instructions")
     
     # Initialize session state for preview message
     if "preview_message" not in st.session_state:
@@ -17,6 +32,14 @@ def chatbot():
         prompt = ("Dear Ms. DIANE, You have a First Visit Consultation at ENT-Head & Neck Surg Ctr - \
                     15C, NUH Medical Centre, Zone B, Level 15, 15c, Lift Lobby B2 on 27 Feb 2025 at 3:45 pm.")
         st.session_state.preview_message = prompt
+    
+    if st.sidebar.button("📷 Insert sample screenshot"):
+        uploaded_file = "imgs/message.jpg"
+        extracted_text = ocr_tool_function(uploaded_file)        
+        st.image(uploaded_file, caption='Uploaded Image', use_container_width=True)
+        st.write("Extracted Text: ", extracted_text)
+        st.session_state.preview_message = extracted_text
+
 
     # Upload image
     uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
@@ -35,7 +58,7 @@ def chatbot():
 
     # Display chat history
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
+        with st.chat_message(message["role"], avatar=user_face):
             st.markdown(message["content"])
 
     # Display preview message if exists
