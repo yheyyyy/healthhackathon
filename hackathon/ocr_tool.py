bebb664f660607f97e5d21d7f6b9652226751031
Onn Yun Hui <onn_yun_hui@aiap.sg>
2025-03-02 19:58:12 +0800

    Add EasyOCR integration for image text extraction

---
+++ hackathon/ocr_tool.py
@@ -0,0 +1,12 @@
+import io
+from PIL import Image
+import easyocr
+
+# OCR function for EasyOCR integration
+def ocr_tool_function(image) -> str:
+    ocr_reader = easyocr.Reader(['en']) 
+    """Extract text from image using EasyOCR."""
+    image = Image.open(io.BytesIO(image_bytes))
+    ocr_result = ocr_reader.readtext(image)
+    extracted_text = ' '.join([text[1] for text in ocr_result])
+    return extracted_text
