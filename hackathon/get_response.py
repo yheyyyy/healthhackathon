17b01504cca07b3d818a2524fdfdc60b6d9fab6e
Onn Yun Hui <onnyunhui@gmail.com>
2025-03-11 22:59:25 +0800

    Enhance get_response.py with structured JSON extraction prompt for appointment details

--- hackathon/get_response.py
+++ hackathon/get_response.py
@@ -38,13 +38,22 @@ def get_response(prompt):
     if response.status_code == 200:
         response_data = response.json()
         content = response_data["choices"][0]["message"]["content"]
+        print(f"content", content)
         return content
     else:
         return f"Error at OpenRouter API: {response.status_code}, {response.text}"
     
 if __name__ == "__main__":
-    message = "Dear Ms. DIANE, You have a First Visit Consultation at ENT-Head & Neck Surg Ctr - 15C, NUH Medical Centre, Zone B, Level 15, 15c, Lift Lobby B2 on 19 Mar 2025 at 3:45 pm"
+    message = """Extract the following text and return in one single JSON format, no need for new line
+    Dear Ms. DIANE, You have a First Visit Consultation at ENT-Head & Neck Surg Ctr - 15C, NUH Medical Centre, Zone B, Level 15, 15c, Lift Lobby B2 on 19 Mar 2025 at 3:45 pm
+    Example JSON Format:
+    {
+    "appointment":
+    "location": 
+    "date": DD MMM YYYY (example: 5 FEB 2024)
+    "time": HH:MM AM/PM (example: 10:30 AM)
+    "description": 
+    }
+    """
     text = get_response(message)
     print(text)
-    
-    
